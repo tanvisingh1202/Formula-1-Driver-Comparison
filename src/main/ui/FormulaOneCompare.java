52b9a7f947c3e53cf6895cc4e1477cbc30421575
tanvisingh <tanvisingh1202@gmail.com>
2023-07-20 08:12:27 -0700

    Phase 1 of Term Project: Reduced method length

--- src/main/ui/FormulaOneCompare.java
+++ src/main/ui/FormulaOneCompare.java
@@ -14,43 +14,36 @@ public class FormulaOneCompare {
         Scanner scan = new Scanner(System.in);
         List<Driver> drivers = new ArrayList<>();
         CompareDriver compareDriver = new CompareDriver();
-
         System.out.println("Formula One Driver Compare");
         System.out.println("Pick a driver of your choice and enter their stats here! Or enter 'X' to exit!");
-
         while (true) {
             System.out.print("Driver name: ");
             String name = scan.nextLine();
             if (name.contentEquals("X")) {
                 break;
             }
-
             System.out.print("Team: ");
             String team = scan.nextLine();
-
             System.out.print("Country: ");
             String country = scan.nextLine();
-
             System.out.print("Career points: ");
             int points = Integer.parseInt(scan.nextLine());
-
             System.out.print("Career wins: ");
             int racewins = Integer.parseInt(scan.nextLine());
-
             System.out.print("Podium finishes: ");
             int topthrees = Integer.parseInt(scan.nextLine());
-
             System.out.print("Pole positions: ");
             int polepositions = Integer.parseInt(scan.nextLine());
-
             System.out.print("Fastest laps: ");
             int fastestlaps = Integer.parseInt(scan.nextLine());
-
             Driver driver = new Driver(name, team, country, points, racewins, topthrees, polepositions, fastestlaps);
             drivers.add(driver);
-
         }
+    }
 
+    public void runafter() {
+        private CompareDriver compareDriver;
+        private List<Driver> drivers
         compareDriver.compareDrivers(drivers);
 
         Driver winner = drivers.stream().max(Comparator.comparingDouble(Driver::getFinalscore)).orElse(null);
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         FormulaOneCompare formulaOneCompare = new FormulaOneCompare();
         formulaOneCompare.run();
+        formulaOnecompare.run
     }
 }
